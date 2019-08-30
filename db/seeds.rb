4c06ec64657e6530da054cd1e05cbde250b50613
Oliver <olliepar01@gmail.com>
2019-08-30 11:56:22 +0100

    Have a working board page where boards can be added

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,4 @@ board = Board.create(title: "test", last_viewed: Time.new.to_s)
 member = Member.create(name: "mr test")
 task = Task.create(status: "test", last_viewed: Time.new.to_s, text: "test", member_id: member.id, board_id: board.id)
 
+12.times {|n| Board.create(title: "Board #{n.to_s}", last_viewed: Time.new.to_s)}
--- server.rb
+++ server.rb
@@ -1,6 +1,17 @@
 require "sinatra"
 require "sinatra/activerecord"
+require_relative "./lib/Kanban.rb"
 
 class Server < Sinatra::Base
+    get "/boards" do
+        erb :boards, locals: {boards: Board.all}
+    end
+    post "/boards" do
+        Board.create(title: params['title'], last_viewed: Time.now.to_s)
+        erb :boards, locals: {boards: Board.all}
+    end
+    get "/members/new" do
+        erb :members
+    end
     
 end
