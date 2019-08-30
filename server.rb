71cdae506c7877baf9c778458427f60a6019fec8
Oliver <olliepar01@gmail.com>
2019-08-30 20:17:41 +0100

    a

--- server.rb
+++ server.rb
@@ -13,5 +13,16 @@ class Server < Sinatra::Base
     get "/members/new" do
         erb :members
     end
-    
+    post "/members/new" do
+        Member.create(name: params['name'])
+        erb :members
+        redirect "/boards"
+    end
+    get "/board/:id" do |id|
+        erb :board, locals: {board: Board.find(id)}
+    end
+    post "/board/:id/tasks" do |id|
+        Task.create(status: 'todo', board_id: id, member_id: 1, text: params['text'])
+        erb :board, locals: {board: Board.find(id)}
+    end
 end
